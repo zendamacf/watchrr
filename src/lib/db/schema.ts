ffb0a0a3c4f06a999f872de599a631e5a0546d5a
Zach Lang <zach.d.lang@gmail.com>
2025-06-24 22:41:06 +1200

    Empties Drizzle

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,7 +1,7 @@
-import { pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
+// import { pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
 
-export const users = pgTable('users', {
-  id: serial('id').primaryKey(),
-  fullName: text('full_name'),
-  phone: varchar('phone', { length: 256 }),
-});
+// export const users = pgTable('users', {
+//   id: serial('id').primaryKey(),
+//   fullName: text('full_name'),
+//   phone: varchar('phone', { length: 256 }),
+// });
