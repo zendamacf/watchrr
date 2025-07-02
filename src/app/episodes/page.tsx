b738504d98bb448c02e0cd1b44fee60dc593c2da
Zach Lang <zach.d.lang@gmail.com>
2025-07-02 21:30:37 +1200

    Styling updates

--- src/app/episodes/page.tsx
+++ src/app/episodes/page.tsx
@@ -4,10 +4,12 @@ import { db } from '@/lib/db';
 import { episodes, tvshows, watcher_episodes } from '@/lib/db/schema';
 import { Card, Flex, Stack, Text, Title } from '@mantine/core';
 import { User } from '@supabase/supabase-js';
+import classNames from 'classnames';
 import { getTimezonesForCountry } from 'countries-and-timezones';
 import { and, eq } from 'drizzle-orm';
 import { DateTime } from 'luxon';
 import { Episode } from './@types';
+import classes from './page.module.css';
 
 export default async function Episodes() {
   return <AuthedPage>{(props) => <EpisodeList {...props} />}</AuthedPage>;
@@ -43,44 +45,60 @@ async function EpisodeList({ user }: { user: User }) {
     const inPast = localDate < DateTime.now();
     return { ...r, episodes: { ...r.episodes, local_date: localDate, in_past: inPast } };
   });
+
   const pastEpisodes = converted.filter((r) => r.episodes.in_past);
   const futureEpisodes = converted.filter((r) => !r.episodes.in_past);
+  const futureDates = futureEpisodes.reduce<Record<string, Episode[]>>((acc, curr) => {
+    const iso = curr.episodes.local_date.toISO()!;
+    if (!acc[iso]) acc[iso] = [];
+    acc[iso].push(curr);
+    return acc;
+  }, {});
 
   return (
     <div>
-      <Stack gap={16}>
-        <Flex wrap={'wrap'} gap={8}>
-          {pastEpisodes.map((r) => (
-            <EpisodeCard key={r.episodes.id} episode={r} />
-          ))}
-        </Flex>
+      <Stack gap={'xl'}>
+        <GroupedEpisodes episodes={pastEpisodes} showDates />
 
-        <Flex wrap={'wrap'} gap={8}>
-          {futureEpisodes.map((r) => (
-            <EpisodeCard key={r.episodes.id} episode={r} />
-          ))}
-        </Flex>
+        {Object.entries(futureDates).map(([iso, episodes]) => (
+          <Stack gap={'sm'} key={iso}>
+            <Title order={2}>
+              <FormattedDate iso={iso} />
+            </Title>
+            <GroupedEpisodes episodes={episodes} />
+          </Stack>
+        ))}
       </Stack>
     </div>
   );
 }
 
-function EpisodeCard({ episode }: { episode: Episode }) {
+function GroupedEpisodes({ episodes, showDates }: { episodes: Episode[]; showDates?: boolean }) {
+  return (
+    <Flex wrap={'wrap'} gap={'md'}>
+      {episodes.map((r) => (
+        <EpisodeCard key={r.episodes.id} episode={r} showDate={showDates} />
+      ))}
+    </Flex>
+  );
+}
+
+function EpisodeCard({ episode, showDate }: { episode: Episode; showDate?: boolean }) {
   const iso = episode.episodes.local_date.toISO()!;
   return (
-    <Card key={episode.episodes.id} w={400}>
+    <Card key={episode.episodes.id} w={400} withBorder>
       <Title order={3} lineClamp={1}>
         {episode.tvshows.name}
       </Title>
-      <Flex gap={8}>
-        <Text
-          fw={'bold'}
-        >{`S${String(episode.episodes.season).padStart(2, '0')}E${String(episode.episodes.episode).padStart(2, '0')}`}</Text>
-        <Text>{episode.episodes.name}</Text>
-      </Flex>
-      <Text c={episode.episodes.in_past ? 'var(--warning)' : undefined}>
-        <FormattedDate iso={iso} />
-      </Text>
+      <Text
+        fw={'bold'}
+      >{`S${String(episode.episodes.season).padStart(2, '0')}E${String(episode.episodes.episode).padStart(2, '0')}`}</Text>
+      <Text>{episode.episodes.name}</Text>
+      {showDate && (
+        <Text className={classNames({ [classes.pastdate]: episode.episodes.in_past })}>
+          <FormattedDate iso={iso} />
+        </Text>
+      )}
     </Card>
   );
 }
