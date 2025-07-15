0b25407ef2730cc44c05ea4b578a714814efae03
Zach Lang <zach.d.lang@gmail.com>
2025-07-15 22:26:47 +1200

    Removes testing code

--- src/app/api/refresh/route.ts
+++ src/app/api/refresh/route.ts
@@ -1,5 +1,6 @@
 import { db } from '@/lib/db';
-import { subscribed_tvshows, tvshows } from '@/lib/db/schema';
+import { movies, subscribed_movies, subscribed_tvshows, tvshows } from '@/lib/db/schema';
+import { refreshMovie } from '@/lib/refresher/movies';
 import { refreshTvShow } from '@/lib/refresher/tvshows';
 import { eq } from 'drizzle-orm';
 import chunk from 'lodash.chunk';
@@ -11,16 +12,16 @@ import { NextResponse } from 'next/server';
  * Refreshing is done in chunks to avoid ratelimiting.
  */
 export async function GET() {
-  //   const subbedMovies = await db
-  //     .selectDistinct({ movie_id: subscribed_movies.movie_id, name: movies.name })
-  //     .from(subscribed_movies)
-  //     .innerJoin(movies, eq(movies.id, subscribed_movies.movie_id))
-  //     .where(eq(subscribed_movies.watched, false))
-  //     .orderBy(movies.name);
+  const subbedMovies = await db
+    .selectDistinct({ movie_id: subscribed_movies.movie_id, name: movies.name })
+    .from(subscribed_movies)
+    .innerJoin(movies, eq(movies.id, subscribed_movies.movie_id))
+    .where(eq(subscribed_movies.watched, false))
+    .orderBy(movies.name);
 
-  //   for (const movieChunk of chunk(subbedMovies, 30)) {
-  //     await Promise.all(movieChunk.map((m) => refreshMovie(m.movie_id)));
-  //   }
+  for (const movieChunk of chunk(subbedMovies, 30)) {
+    await Promise.all(movieChunk.map((m) => refreshMovie(m.movie_id)));
+  }
 
   const subbedShows = await db
     .selectDistinct({ tvshow_id: subscribed_tvshows.tvshow_id, name: tvshows.name })
