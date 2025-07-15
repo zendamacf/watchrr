6f94cf0b330367f5908f5a49c6cdb28fa763c083
Zach Lang <zach.d.lang@gmail.com>
2025-07-15 21:11:37 +1200

    Adds Sentry for error monitoring

---
+++ src/instrumentation-client.ts
@@ -0,0 +1,9 @@
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: 'https://a42d84a2023c9b94b8feb5d7f5b69479@o4509541345591296.ingest.de.sentry.io/4509671716749392',
+  tracesSampleRate: 1,
+  debug: false,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
