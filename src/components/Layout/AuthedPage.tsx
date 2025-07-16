dd00787e3d6999f2f88364f71176199e77a29f1f
Zach Lang <zach.d.lang@gmail.com>
2025-07-16 17:22:17 +1200

    Fixes typecheck error

--- src/components/Layout/AuthedPage.tsx
+++ src/components/Layout/AuthedPage.tsx
@@ -1,11 +1,15 @@
 import { guardUser } from '@/utils/auth';
+import { SupabaseSafeUser } from '@/utils/supabase/safeSession';
 import { Container, Space } from '@mantine/core';
-import { User } from '@supabase/supabase-js';
 import { redirect } from 'next/navigation';
 import { ComponentType } from 'react';
 import { SiteHeader } from '../SiteHeader';
 
-export async function AuthedPage({ children: Child }: { children: ComponentType<{ user: User }> }) {
+export async function AuthedPage({
+  children: Child,
+}: {
+  children: ComponentType<{ user: SupabaseSafeUser }>;
+}) {
   const user = await guardUser();
   if (!user) redirect('/signin');
 
